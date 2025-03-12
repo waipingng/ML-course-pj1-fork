58d24cf03e31636ff004cddfdfa1b8107c6b5fbc
王毓晨 <wangyuchen@wireless-10-148-71-1.public.utexas.edu>
2025-03-12 12:22:03 -0500

    fix best hyperparameters

--- code/merge_data.py
+++ code/merge_data.py
@@ -7,8 +7,8 @@ df2 = pd.read_csv("cleaned_data/income_combined.csv")
 df1.rename(columns={"LocationDesc": "County"}, inplace=True)
 df1.rename(columns={"LocationAbbr": "State Code"}, inplace=True)
 df2.rename(columns={"Label (Grouping)": "County"}, inplace=True)
-merged_df = pd.merge(df1, df2, on=["Year", "County", "State Code"], how="inner")
+merged_df = pd.merge(df1, df2, on=["Year", "County", "State Code"], how="left")
 
 merged_df = merged_df.loc[(merged_df["Sex"] == "Overall") & (merged_df["ethnicity"] == "Overall")]
 merged_df = merged_df.drop(columns=["GeographicLevel", "Data_Value_Unit", "Data_Value_Type", "LocationID", "Y_lat", "X_lon", "Georeference"])
-merged_df.to_csv("cleaned_data/merged_data.csv", index=False)
+merged_df.to_csv("cleaned_data/merged_data_1.csv", index=False)
